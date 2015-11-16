b9a6fe9c897152ee1e12f1893479f816fdbdf6f9
Terry Bu <burea1124@gmail.com>
2015-11-16 13:33:43 -0500

    First commit

---
+++ PaddedUILabel/PaddedLabel.swift
@@ -0,0 +1,39 @@
+//
+//  PaddedLabel.swift
+//  In2-PI-Swift
+//
+//  Created by Terry Bu on 9/7/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class PaddedLabel: UILabel {
+
+    /*
+    
+    This is to add left and right padding to the text inside the label
+    drawTextInRect with UIEdgeInsetsInsetRect is not enough because it truncates the lable afterwards
+    This basically handles the resizing of the label to accommodate the padding
+    
+    Don't forget to actually "set" the edgeInsets in your viewcontroller when dealing with your padded label. Right now, it's set to zero.
+    */
+
+    
+    var edgeInsets: UIEdgeInsets = UIEdgeInsetsZero
+    
+    override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        var rect = super.textRectForBounds(UIEdgeInsetsInsetRect(bounds, edgeInsets), limitedToNumberOfLines: numberOfLines)
+        
+        rect.origin.x -= edgeInsets.left
+        rect.origin.y -= edgeInsets.top
+        rect.size.width  += (edgeInsets.left + edgeInsets.right);
+        rect.size.height += (edgeInsets.top + edgeInsets.bottom);
+        
+        return rect
+    }
+    
+    override func drawTextInRect(rect: CGRect) {
+        super.drawTextInRect(UIEdgeInsetsInsetRect(rect, edgeInsets))
+    }
+}
--- PaddedUILabel/ViewController.swift
+++ PaddedUILabel/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label = PaddedLabel()
+        label.frame = CGRectMake(20, view.center.y, 0, 0)
+        label.text = "terry bu standup comedy"
+        label.textAlignment = NSTextAlignment.Center
+        label.layer.borderColor = UIColor.lightGrayColor().CGColor
+        label.layer.borderWidth = 1.5
+        label.edgeInsets = UIEdgeInsetsMake(15, 30, 15, 30)
+        label.sizeToFit()
+        label.textColor = UIColor.grayColor()
+        
+        view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
